4b8382a07d62b5164870b7b9107f773bdd459642
xiaox232 <xiaox232@morris.umn.edu>
2015-02-03 19:25:46 -0600

    Added comments to describe functions.

--- public/javascript/main.controller.js
+++ public/javascript/main.controller.js
@@ -57,6 +57,8 @@ mainApp.controller('mainCtrl', function($scope){
 mainApp.controller('GPACtrl', function($scope) {
     console.log("controller loaded!");
 
+    //we added numberField and letterField as well as numberData and letterData, so we can push credits and letter grade
+    //into data list.
     $scope.courseField = "";
     $scope.courseData = [];
     $scope.numberData = [];
@@ -78,7 +80,7 @@ mainApp.controller('GPACtrl', function($scope) {
         }
     };
 
-
+    //contains function, to check if a object is in array
     $scope.contains = function(a,obj) {
         for (var i =0; i < a.length; i++) {
             if (a[i] === obj) {
@@ -88,6 +90,7 @@ mainApp.controller('GPACtrl', function($scope) {
         return false;
     };
 
+    //validation function, to check if the inputs are valid.
     $scope.validation = function () {
         var letters = ["A","B","C","D","F"];
        if ($scope.numberField == 0) {
@@ -95,7 +98,7 @@ mainApp.controller('GPACtrl', function($scope) {
            return false;
        }
         else if (!$scope.contains(letters,$scope.letterField)) {
-           console.log("works here");
+//           console.log("works here");
            alert("error: invalid letter grade");
            return false;
        }
@@ -107,6 +110,9 @@ mainApp.controller('GPACtrl', function($scope) {
     $scope.coursesInList = function () {
         return $scope.courseData.length;
     };
+
+    //changed it so that when the user remove a course, the numberdata and letterdata removed as well, and re-calls function
+    //GPAcalc().
     $scope.removeCourseData = function (index) {
         $scope.courseData.splice(index, 1);
         $scope.numberData.splice(index, 1);
@@ -114,6 +120,8 @@ mainApp.controller('GPACtrl', function($scope) {
         $scope.GPAcalc();
     };
 
+
+    //Another helper function that can change a given letter grade to number grade.
     $scope.ChangeToNumber = function (str) {
         var numGrade;
         switch (str) {
@@ -134,31 +142,33 @@ mainApp.controller('GPACtrl', function($scope) {
                 break;
         }
         numGrade = number;
-        console.log("NumGrade");
-        console.log(numGrade);
+//        console.log("NumGrade");
+//        console.log(numGrade);
         return numGrade;
 
     };
 
+    //This is our function to calculate GPA.
     $scope.GPAcalc = function () {
         var letter = $scope.letterData;
         var numb = $scope.numberData;
-        console.log("~~~~~~~~~ Gpa calc stuff ~~~~");
-        console.log(letter[0]);
-        console.log(numb[0]);
+//        console.log("~~~~~~~~~ Gpa calc stuff ~~~~");
+//        console.log(letter[0]);
+//        console.log(numb[0]);
         var result;
         var top = 0;
         var bottom = 0;
                for (i = 0; i < numb.length; i++) {
                     bottom += Number(numb[i].number);
-                   console.log(numb[i].number);
+//                   console.log(numb[i].number);
                    top = top + numb[i].number * $scope.ChangeToNumber(letter[i].letter);
                     result = top / bottom;
                 }
-        console.log("bottom: " + bottom);
+//        console.log("bottom: " + bottom);
         return result;
     };
 
+    //This function will change the color of words depends on the GPA score.
     $scope.colorCode = function () {
         if ($scope.GPAcalc() < 2.0) {
            return {"color": 'red'};
